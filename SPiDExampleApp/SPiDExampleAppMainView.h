bc644bbcb9e5ba04e417fa94c3a0bc641c18cef3
Mikael Lindström <mikaellindstrom@Netlight-n81.local>
2012-09-11 15:09:45 +0200

    Added simple layout for test app

---
+++ SPiDExampleApp/SPiDExampleAppMainView.h
@@ -0,0 +1,15 @@
+//
+//  SPiDExampleAppMainView.h
+//  SPiDSDK
+//
+//  Created by Mikael Lindström on 9/11/12.
+//  Copyright (c) 2012 Mikael Lindström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPiDExampleAppMainView : UIViewController <UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
---
+++ SPiDExampleApp/SPiDExampleAppMainView.m
@@ -0,0 +1,15 @@
+//
+//  SPiDExampleAppMainView.m
+//  SPiDSDK
+//
+//  Created by Mikael Lindström on 9/11/12.
+//  Copyright (c) 2012 Mikael Lindström. All rights reserved.
+//
+
+#import "SPiDExampleAppMainView.h"
+
+@implementation SPiDExampleAppMainView
+
+@synthesize loginButton = _loginButton;
+
+@end
