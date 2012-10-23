f0b2dd28fe6eaa3a0e292fc69c747b6fe1ec43b0
mikaellindstrom <mikael.lindstrom@netlight.com>
2012-10-23 21:43:50 +0200

    * Added SPiDExampleAppDelegate

---
+++ SPiDExampleApp/SPiDExampleAppDelegate.m
@@ -0,0 +1,57 @@
+//
+//  AppDelegate.m
+//  SPiDExampleApp
+//
+//  Created by Mikael Lindström on 9/11/12.
+//  Copyright (c) 2012 Mikael Lindström. All rights reserved.
+//
+
+static NSString *const ClientID = @"your-client-id";
+static NSString *const ClientSecret = @"your-client-secret";
+static NSString *const AppURLScheme = @"your-app-url";
+static NSString *const ServerURL = @"your-spidserver-url";
+
+#import "SPiDExampleAppDelegate.h"
+
+@implementation SPiDExampleAppDelegate
+
+@synthesize window = _window;
+@synthesize navigationController = _navigationController;
+@synthesize loginView = _loginView;
+@synthesize mainView = _mainView;
+@synthesize useWebView = _useWebView;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[SPiDClient sharedInstance] setClientID:ClientID
+                             andClientSecret:ClientSecret
+                             andAppURLScheme:AppURLScheme
+                                andServerURL:[NSURL URLWithString:ServerURL]];
+    [[SPiDClient sharedInstance] setWebViewInitialHTML:@"<html><body>Loading SPiD login page</body></html>"];
+
+    [self setUseWebView:YES]; // As default, logout as logged in through webview
+
+    // Initialize all views
+    [self setLoginView:[[SPiDLoginViewController alloc] initWithNibName:@"SPiDLoginView" bundle:nil]];
+    [self setMainView:[[SPiDMainViewController alloc] initWithNibName:@"SPiDMainView" bundle:nil]];
+
+    [self setWindow:[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]]];
+    [self setNavigationController:[[UINavigationController alloc] initWithRootViewController:[self loginView]]];
+    [[self window] setRootViewController:[self navigationController]];
+
+    // Access token was saved in keychain
+    if ([[SPiDClient sharedInstance] isAuthorized]) {
+        [_navigationController pushViewController:[self mainView] animated:NO];
+    }
+
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    BOOL didSPiDHandleURL = [[SPiDClient sharedInstance] handleOpenURL:url];
+
+    // Always return that the URL was handled by the application
+    return YES;
+}
+
+@end
