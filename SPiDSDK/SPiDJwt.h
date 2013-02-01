9bb1a73c2dba7ac623b25eae68a46c19ab75f0e6
mikaellindstrom <mikael.lindstrom@gmail.com>
2013-02-01 15:04:39 +0100

    * Added JWT class

---
+++ SPiDSDK/SPiDJwt.h
@@ -0,0 +1,30 @@
+//
+//  SPiDJwt
+//  SPiDSDK
+//
+//  Created by mikaellindstrom on 1/28/13.
+//  Copyright (c) 2012 Schibsted Payment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString *const JSON_WEB_ALGORITHM_HS256 = @"HS256";
+static NSString *const JSON_WEB_TOKEN_TYP_JWT = @"JWT";
+
+@interface SPiDJwt : NSObject
+
+// Header
+@property(strong, nonatomic) NSString *alg;
+@property(strong, nonatomic) NSString *typ;
+
+// MUST requirements for JWT over OAuth 2.0
+@property(strong, nonatomic) NSString *iss; // issuer (facebook appid)
+@property(strong, nonatomic) NSString *sub; // authorization
+@property(strong, nonatomic) NSString *aud; // intended url (SPiDTokenURL)
+@property(strong, nonatomic) NSDate *exp;   // expiration date for the token
+
+// SPiD Specific
+@property(strong, nonatomic) NSString *tokenType;  // type of token (facebook)
+@property(strong, nonatomic) NSString *tokenValue; // the actual token
+
+@end
---
+++ SPiDSDK/SPiDJwt.m
@@ -0,0 +1,86 @@
+//
+//  SPiDJwt
+//  SPiDSDK
+//
+//  Created by mikaellindstrom on 1/28/13.
+//  Copyright (c) 2012 Schibsted Payment. All rights reserved.
+//
+
+#import "SPiDJwt.h"
+#import "NSData+Base64.h"
+#import "SPiDClient.h"
+#import "NSString+Crypto.h"
+
+@implementation SPiDJwt
+
+@synthesize iss = _iss;
+@synthesize sub = _sub;
+@synthesize aud = _aud;
+@synthesize exp = _exp;
+@synthesize tokenType = _tokenType;
+@synthesize tokenValue = _tokenValue;
+
+- (NSString *)encodedJwtString {
+    if (![self validateJwt]) {
+        return nil;
+    }
+
+    NSError *jsonError;
+    NSDictionary *headerDictionary = [[NSDictionary alloc] initWithObjectsAndKeys:JSON_WEB_ALGORITHM_HS256, @"alg",
+                                                                                  JSON_WEB_TOKEN_TYP_JWT, @"typ", nil];
+    NSData *headerJson = [NSJSONSerialization dataWithJSONObject:headerDictionary options:0 error:&jsonError];
+    NSString *header = [headerJson base64EncodedString];
+    if (jsonError != nil) {
+        SPiDDebugLog(@"Error encoding JWT header");
+        return nil;
+    }
+
+    NSDictionary *claimDictionary = [[NSDictionary alloc] initWithObjectsAndKeys:self.iss, @"iss",
+                                                                                 self.sub, @"sub",
+                                                                                 self.aud, @"aud",
+                                                                                 self.exp, @"exp",
+                                                                                 self.tokenType, @"token_type",
+                                                                                 self.tokenValue, @"token_value",
+                                                                                 nil];
+
+    NSData *claimJson = [NSJSONSerialization dataWithJSONObject:claimDictionary options:0 error:&jsonError];
+    NSString *claim = [claimJson base64EncodedString];
+    if (jsonError != nil) {
+        SPiDDebugLog(@"Error encoding JWT claim");
+        return nil;
+    }
+
+    NSString *payload = [NSString stringWithFormat:@"%@.%@", header, claim];
+    NSString *signature = [payload hmacSHA256withKey:[[SPiDClient sharedInstance] sigSecret]];
+    return [NSString stringWithFormat:@"%@.%@.%@", header, claim, signature];
+}
+
+- (BOOL)validateJwt {
+    if (self.iss == nil) {
+        SPiDDebugLog(@"JWT is missing value for iss");
+        return NO;
+    }
+    if (self.sub == nil) {
+        SPiDDebugLog(@"JWT is missing value for sub");
+        return NO;
+    }
+    if (self.aud == nil) {
+        SPiDDebugLog(@"JWT is missing value for aud");
+        return NO;
+    }
+    if (self.exp == nil) {
+        SPiDDebugLog(@"JWT is missing value for exp");
+        return NO;
+    }
+    if (self.tokenType == nil) {
+        SPiDDebugLog(@"JWT is missing value for token type");
+        return NO;
+    }
+    if (self.tokenValue) {
+        SPiDDebugLog(@"JWT is missing value for token value");
+        return NO;
+    }
+    return YES;
+}
+
+@end
