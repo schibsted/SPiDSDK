ddc751f5c989e134424f8f3621c533c69f1a4c4a
mikaellindstrom <mikael.lindstrom@gmail.com>
2013-09-13 16:01:43 +0200

    Fixed syntax error

--- SPiDSDK/SPiDClient.h
+++ SPiDSDK/SPiDClient.h
@@ -5,7 +5,7 @@
 //  Copyright (c) 2012 Schibsted Payment. All rights reserved.
 //
 
-#define SPID_IOS_SDK_VERSION_STRING @"1.2.2";
+#define SPID_IOS_SDK_VERSION_STRING @"1.2.2"
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
