95c3ceba8b4a51169f5fb02ec79adeb2abe91ba8
Joakim Gyllström <joakim@backslashed.se>
2016-05-24 12:22:13 +0200

    Fixed pod lib lint

--- SPiDSDK/SPiDSDK.h
+++ SPiDSDK/SPiDSDK.h
@@ -14,17 +14,20 @@ FOUNDATION_EXPORT double SPiDVersionNumber;
 //! Project version string for SPiD.
 FOUNDATION_EXPORT const unsigned char SPiDVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SPiD/PublicHeader.h>
+// In this header, you should import all the public headers of your framework.
 
-#import <SPiD/NSError+SPiD.h>
-#import <SPiD/SPiDAccessToken.h>
-#import <SPiD/SPiDClient.h>
-#import <SPiD/SPiDJwt.h>
-#import <SPiD/SPiDKeychainWrapper.h>
-#import <SPiD/SPiDRequest.h>
-#import <SPiD/SPiDResponse.h>
-#import <SPiD/SPiDStatus.h>
-#import <SPiD/SPiDTokenRequest.h>
-#import <SPiD/SPiDUser.h>
-#import <SPiD/SPiDUtils.h>
-#import <SPiD/SPiDWebView.h>
+#import "NSError+SPiD.h"
+#import "SPiDAccessToken.h"
+#import "SPiDClient.h"
+#import "SPiDJwt.h"
+#import "SPiDKeychainWrapper.h"
+#import "SPiDRequest.h"
+#import "SPiDResponse.h"
+#import "SPiDStatus.h"
+#import "SPiDTokenRequest.h"
+#import "SPiDUser.h"
+#import "SPiDUtils.h"
+
+#if TARGET_OS_IOS
+    #import "SPiDWebView.h"
+#endif
